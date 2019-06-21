4662dcb201bc9c0f2cb5214f87ffb0e5d8b377d3
GGG1235 <gxk1910@outlook.com>
2019-06-21 23:32:05 +0800

    1.TwoSum cpp

--- leetcode/C++/TwoSum.cpp
+++ leetcode/C++/TwoSum.cpp
@@ -1,16 +1,73 @@
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
-class Solution
+class Solution1
 {
 public:
     vector<int> twoSum(vector<int> &nums, int target)
     {
+        int length=nums.size();
+        vector<int> res;
+        for (int i = 0; i < length; i++)
+        {
+            int num = target - nums[i];
+            for (int j = i+1; j < length; j++)
+            {
+                if(num==nums[j]){
+                    res.push_back(i);
+                    res.push_back(j);
+                    return res;
+                }
+            }
+            
+        }
+        return res;
     }
 };
 
+class Solution2
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        int length = nums.size();
+        map<int,int> dict;
+        vector<int> res;
+        for (int i = 0; i < length; i++)
+        {
+            int num = target - nums[i];
+            if (dict.count(num)!=0)
+            {
+                res.push_back(dict[num]);
+                res.push_back(i);
+                return res;
+            }
+            dict[nums[i]]=i;
+        }
+        return res;
+    }
+
+};
+
 int main(int argc,char **argv,char **envp){
+    int n;
+    while (cin>>n)
+    {
+        vector<int> nums;
+        int num,target;
+        for(int i=0;i<n;i++){
+            cin>>num;
+            nums.push_back(num);
+        }
+        cin>>target;
+        // vector<int> res = Solution1().twoSum(nums, target);
+        vector<int> res=Solution2().twoSum(nums,target);
+        for(vector<int>::iterator iter=res.begin();iter!=res.end();iter++){
+            cout<<*iter<<endl;
+        }
+    }
     return 0;
 }
--- leetcode/Python/TwoSum.py
+++ leetcode/Python/TwoSum.py
@@ -1,23 +1,39 @@
 #!/usr/local/bin/python3.7
 
-class Solution:
+# O(n^2)
+# 双重循环
+class Solution1:
     def twoSum(self, nums: list, target: int) -> list:
         res=list()
-        l=len(nums)
-        for i in range(0,l):
-            n=target-nums[i]
-            for j in range(i+1,l):
-                if n==nums[j]:
+        length=len(nums)
+        for i in range(0, length):
+            num=target-nums[i]
+            for j in range(i+1, length):
+                if num==nums[j]:
                     res.append(i)
                     res.append(j)
                     break
         return res
 
+
+# O(n)
+# 使用字典,把扫过的num索引存入字典
+class Solution2:
+    def twoSum(self, nums: list, target: int) -> list:
+        dic,length=dict(),len(nums)
+        for i in range(0,length):
+            num = target - nums[i]
+            if num in dic.keys():
+                return [dic[num], i]
+            dic[nums[i]] = i
+        return list()
+
 def main():
     while 1:
         nums=[int(x) for x in input().split()]
         target=int(input())
-        print(Solution().twoSum(nums,target))
+        # print(Solution1().twoSum(nums,target))
+        print(Solution2().twoSum(nums, target))
 
 if __name__ == "__main__":
     main()
