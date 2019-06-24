410e7a631dd49c5d2883ac881cfec926a4c787d5
GGG1235 <gxk1910@outlook.com>
2019-06-24 10:36:01 +0800

    剑指 offer

---
+++ 剑指offer/Java/PrintListFromTailToHead.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+class ListNode {
+    int val;
+    ListNode next = null;
+    
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+
+class Solution {
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+
+    }
+}
+
+public class PrintListFromTailToHead{
+    public static void main(String[] args) {
+        
+    }
+}
