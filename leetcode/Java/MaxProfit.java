ba3d45a20606af7a3a133b32aeafa80f6b032ff3
GGG1235 <gxk1910@outlook.com>
2019-06-25 11:21:54 +0800

    leetcode 购买股票的最佳时机 JAVA

--- leetcode/Java/MaxProfit.java
+++ leetcode/Java/MaxProfit.java
@@ -1,6 +1,25 @@
 import java.util.Scanner;
 
 /*
+ * 暴力破解,双重循环
+ */
+class Solution1 {
+    public int maxProfit(int[] prices) {
+        int temp = 0,result = 0;
+        int length = prices.length;
+        for (int i = 0; i < length-1; i++) {
+            for (int j = i+1; j < length; j++) {
+                temp = prices[j]-prices[i];
+                if(temp > result){
+                    result = temp;
+                }
+            }
+        }
+        return result;
+    }
+}
+
+/*
  * 动态规划
  * 求出序列两两之间的差值,然后开始累加,如果累加和为负数,说明这不是最佳适合的买入点,可以清零,从后继续开始,找到累加和的最大值,更新即可
  * 时间复杂度为0(N),空间复杂度为0(1)
@@ -30,7 +49,8 @@ public class MaxProfit{
             for (int i = 0; i < n; i++) {
                 prices[i]=in.nextInt();
             }
-            System.out.println(new Solution2().maxProfit(prices));
+            System.out.println(new Solution1().maxProfit(prices));
+            // System.out.println(new Solution2().maxProfit(prices));
         }
     }
 }
